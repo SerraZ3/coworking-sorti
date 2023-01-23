875805b08f40c6ea706da0c61a677fc94a7a59e3
SerraZ3 <serra.henrique3@gmail.com>
2023-01-23 11:57:51 -0300

    feat: migrando de vite para react

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
+import ReactGA from "react-ga";
+const TRACKING_ID = "G-ML4LGCNR96"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
