b3877ca8574b341b53aac4a2c7bcf9eccfd6d728
TawanAlves <tas.tawan@.com>
2023-01-23 19:26:01 -0300

    atualizando build

---
+++ client/build/precache-manifest.e378e64fa760e036a4dd5572259e03ec.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f594e6250d9f6688bb6",
+    "url": "/static/js/main.4f594e62.chunk.js"
+  },
+  {
+    "revision": "6e839c288029d118e0ad",
+    "url": "/static/css/1.23be2b8e.chunk.css"
+  },
+  {
+    "revision": "6e839c288029d118e0ad",
+    "url": "/static/js/1.6e839c28.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b6ee424564f5684ce54d24953be34e07",
+    "url": "/static/media/sorti.b6ee4245.svg"
+  },
+  {
+    "revision": "1aa08520271f1c1cf822fe130ba22b1b",
+    "url": "/static/media/instagramIcon.1aa08520.svg"
+  },
+  {
+    "revision": "01de33cf2fa4edf2524e166eb8f7e1b2",
+    "url": "/static/media/whatsappIcon.01de33cf.svg"
+  },
+  {
+    "revision": "1e15ddef3e908f5778570641a5af3bc2",
+    "url": "/static/media/cellphone2.1e15ddef.svg"
+  },
+  {
+    "revision": "6041fbf0a0284761f0bd218e5421c5f2",
+    "url": "/static/media/cellphone.6041fbf0.svg"
+  },
+  {
+    "revision": "ca58d3f5104e776d6a1db0e919b9c96b",
+    "url": "/static/media/instagram.ca58d3f5.svg"
+  },
+  {
+    "revision": "741f773917cc2e980c1d728a6d403f09",
+    "url": "/static/media/whatsapp.741f7739.svg"
+  },
+  {
+    "revision": "72e32a8ed93d904d9c3302620ecb9e83",
+    "url": "/static/media/sala1.72e32a8e.jpg"
+  },
+  {
+    "revision": "1be6d6c67b90b4d988dff646d09a1ffd",
+    "url": "/static/media/sala2.1be6d6c6.jpg"
+  },
+  {
+    "revision": "81027e31ca7bbc0128952de6bbb69ba7",
+    "url": "/static/media/slide1.81027e31.jpg"
+  },
+  {
+    "revision": "60759b32ce544bf5ad9ec99a61829721",
+    "url": "/static/media/slide2.60759b32.jpg"
+  },
+  {
+    "revision": "bd46cd65ed3fd28d3b315397e512f414",
+    "url": "/static/media/slide3.bd46cd65.jpg"
+  },
+  {
+    "revision": "0828ef6a777b91fca14298a2a34b7143",
+    "url": "/static/media/slide4.0828ef6a.jpg"
+  },
+  {
+    "revision": "5425826f3ca8c62c5b687211ee4dfcad",
+    "url": "/static/media/slide5.5425826f.jpg"
+  },
+  {
+    "revision": "3c8f55d8096787473de50f1697f67df1",
+    "url": "/index.html"
+  }
+];
